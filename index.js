536031760fe595db3affc5da3984499f18e88f7f
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-01-31 20:52:48 +0300

    created a book router using HTTP get

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 const express = require('express');
 
 const app = express();
-
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    const responce = { book: 'JavaScript the Good parts' };
+    res.json(responce);
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to RESTful web services.');
