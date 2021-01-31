f96ea64aa3a3141041235af26105af63aa43bf3c
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-01-31 21:48:43 +0300

    connected mongoDB and added data from booksjson and configuard using mongoose

---
+++ booksJson.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+db.books.insert([
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'JavaScript the Good Parts',
+    genre: 'Educational',
+    author: 'Douglas Clockford',
+    read: true
+  },
+  {
+    title: 'The Caucacian Chalk Circle',
+    genre: 'Epic Theatre',
+    author: 'Bertolt Brecht',
+    read: true
+  },
+  {
+    title: 'The River and the source',
+    genre: 'Literary fiction',
+    author: 'Margaret Ogola',
+    read: true
+  },
+  {
+    title: 'Treasure Island',
+    genre: 'Adventure Fiction',
+    author: 'Robert Louis Stevenson',
+    read: false
+  },
+  {
+    title: 'The Pearl',
+    genre: 'Novel, Novella, fiction',
+    author: 'John Steinbeck',
+    read: false
+  }
+]);
---
+++ models/bookModel.js
@@ -0,0 +1,16 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const bookModel = new Schema(
+  {
+    title: { type: String },
+    author: { type: String },
+    genre: { type: String },
+    read: { type: Boolean, default: false }
+  }
+);
+
+module.exports = mongoose.model('Book', bookModel);
