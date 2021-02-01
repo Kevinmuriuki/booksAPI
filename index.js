86132f5df5ed74b5aedb8ae03b1b8912ebcbf406
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-02-01 07:15:08 +0300

    Created two routes one that queries a list of items and the other a single item

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    Book.find((err, books) => {
-      if(err) {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
         return res.send(err);
       }
       return res.json(books);
+    });
+  });
+
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
     });
   });
 
