79a0bc2da1fe367f53b0a10f542dc30f2d2a5409
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-02-01 09:14:58 +0300

    implemented HTTP patch

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-param-reassign */
 /* eslint-disable linebreak-style */
 const express = require('express');
@@ -26,7 +28,7 @@ function routes(Book) {
     });
 
   // midleware
-  bookRouter('/books/:bookId', (req, res, next) => {
+  bookRouter.use('/books/:bookId', (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) {
         return res.send(err);
@@ -47,8 +49,29 @@ function routes(Book) {
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
-      return res.json(book);
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     });
 
   return bookRouter;
